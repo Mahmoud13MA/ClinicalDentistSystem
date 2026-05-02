506a7572e3ef4dc6261d365b01a50dfba52f1d5b
Omar Yousry <152629913+omar23py@users.noreply.github.com>
2026-05-02 14:43:29 +0300

    Add AutoMapper profile for Radiology module DTO mappings
    Introduced RadiologyMappingProfile to configure mappings between Radiology domain models and their related DTOs. This streamlines object transformation for Equipment, ImagingAppointment, Radiologist, and Report entities within the module.

---
+++ Modules/Radiology/MappingProfiles/RadiologyMappingProfile.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using clinical.APIs.Modules.Radiology.DTOs;
+using Radiology.Models;
+
+namespace clinical.APIs.Modules.Radiology.MappingProfiles
+{
+    public class RadiologyMappingProfile : Profile
+    {
+
+        public RadiologyMappingProfile() {
+            // Equipment
+            CreateMap<Equipment, EquipmentResponse>();
+            CreateMap<EquipmentCreateRequest, Equipment>();
+            CreateMap<EquipmentUpdateRequest, Equipment>();
+            CreateMap<Equipment, EquipmentBasicInfo>();
+
+            //ImagingAppointment
+            CreateMap<ImagingAppointment, ImagingAppointmentResponse>();
+            CreateMap<ImagingAppointmentCreateRequest, ImagingAppointment>();
+            CreateMap<ImagingAppointmentUpdateRequest, ImagingAppointment>();
+            CreateMap<ImagingAppointment, ImagingAppointmentBasicInfo>();
+
+
+
+            //Radiologist
+            CreateMap<Radiologist, RadiologistResponse>();
+            CreateMap<RadiologistCreateRequest, Radiologist>();
+            CreateMap<RadiologistUpdateRequest, Radiologist>();
+            CreateMap<Radiologist, RadiologistLoginResponse>();
+            CreateMap<Radiologist, RadiologistBasicInfo>();
+
+
+            // Report
+            CreateMap<Report, ReportResponse>();
+            CreateMap<ReportCreateRequest, Report>();
+            CreateMap<ReportUpdateRequest, Report>();
+            CreateMap<Report, ReportBasicInfo>();
+        }
+
+    }
+}
